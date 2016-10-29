5d71286864936bfc87397426d8fc594441bf1c53
emanlapig <emmanlapig@gmail.com>
2016-10-29 15:18:19 -0400

    implemented dual-state keys

--- Main.js
+++ Main.js
@@ -13,7 +13,6 @@ var width = Math.floor( window.innerWidth/unit )
 	, height = Math.floor( window.innerHeight/unit )
 	, total = width * height
 	, world = []
-	, world2 = []
 	, fps = 9
 	, playing
 	, canvas
@@ -34,6 +33,11 @@ var width = Math.floor( window.innerWidth/unit )
 	, eggs4 = []
 	, eggs5 = [];
 
+// DUAL-STATE KEYS: previously, we had to store 2 copies of the world--one to read and one to act on for the next iteration. the dual-state key pattern allows us to read and act on a single array by preserving the previous state of cells we've already iterated over. this works with a standard array only if the cells have a maximum of 10 possible states, so we can use keys 0-9.
+// cases: cells can either go from alive (4) to dead (3), dead (0-3) to more dead (n-1), or dead (0-3) to alive (4)
+// a.4->4, b.4->3, c.3->4, d.3->2, e.2->4, f.2->1, g.1->4, h.1->0, i.0->4, j.0->0
+var key_to = [ 4, 3, 4, 2, 4, 1, 4, 0, 4, 0 ];
+var key_from = [ 4, 4, 3, 3, 2, 2, 1, 1, 0, 0 ];
 
 var deadColor2 = [
 	Math.floor( Number( deadColor[0] + (bgColor[0] - deadColor[0])*(1/3) ) ),
@@ -56,13 +60,12 @@ var goL = {
 		for ( var i=0; i<total; i++ ) {
 			var rand = ( Math.floor( Math.random() * 2 ) == 0 );
 			if (rand) {
-				world.push(4);
+				world.push(9);
 			} else {
 				world.push(0);
 			}
 			if ( i === total-1 ) {
 				console.log("ready");
-				world2 = world.slice();
 				goL.render();
 				goL.start();
 			}
@@ -74,7 +77,8 @@ var goL = {
 		for ( var i=0; i<world.length; i++ ) {
 			var col = Math.floor( i/width ) * unit;
 			var row = Math.floor( i%width ) * unit;
-			switch ( world[i] ) {
+			var to = key_to[ world[i] ];
+			switch ( to ) {
 				case 0:
 					ctx.fillStyle = "rgb("+bgColor[0]+","+bgColor[1]+","+bgColor[2]+")";
 					break;
@@ -96,11 +100,8 @@ var goL = {
 		if ( !fps ) {
 			window.requestAnimationFrame( goL.parse_world );
 		}
-		world2 = [];
 	},
 	parse_world: function() {
-		world2 = [];
-		world2 = world.slice();
 		for ( var i=0; i<world.length; i++ ) {
 			var i1 = i-1
 				, i2 = i+1
@@ -111,14 +112,14 @@ var goL = {
 				, i7 = i+width-1
 				, i8 = i+width+1;
 
-			var p1 = ( world[i1] >= 0 )? ( world[i1] ) : 0
-				, p2 = ( world[i2] >= 0 )? ( world[i2] ) : 0
-				, p3 = ( world[i3] >= 0 )? ( world[i3] ) : 0
-				, p4 = ( world[i4] >= 0 )? ( world[i4] ) : 0
-				, p5 = ( world[i5] >= 0 )? ( world[i5] ) : 0
-				, p6 = ( world[i6] >= 0 )? ( world[i6] ) : 0
-				, p7 = ( world[i7] >= 0 )? ( world[i7] ) : 0
-				, p8 = ( world[i8] >= 0 )? ( world[i8] ) : 0;
+			var p1 = ( world[i1] >= 0 )? ( key_from[ world[i1] ] ) : 0
+				, p2 = ( world[i2] >= 0 )? ( key_to[ world[i2] ] ) : 0
+				, p3 = ( world[i3] >= 0 )? ( key_from[ world[i3] ] ) : 0
+				, p4 = ( world[i4] >= 0 )? ( key_from[ world[i4] ] ) : 0
+				, p5 = ( world[i5] >= 0 )? ( key_from[ world[i5] ] ) : 0
+				, p6 = ( world[i6] >= 0 )? ( key_to[ world[i6] ] ) : 0
+				, p7 = ( world[i7] >= 0 )? ( key_to[ world[i7] ] ) : 0
+				, p8 = ( world[i8] >= 0 )? ( key_to[ world[i8] ] ) : 0;
 
 			if ( p1>3 ) { p1 = 1; } else { p1 = 0; };
 			if ( p2>3 ) { p2 = 1; } else { p2 = 0; };
@@ -131,18 +132,61 @@ var goL = {
 
 			var sum = p1 + p2 + p3 + p4 + p5 + p6 + p7 + p8;
 
-			if ( world[i] < 4 ) {
-				if ( sum === 3 ) {
-					world2[i] = 4;
+			var to = key_to[ world[i] ];
+			var from = key_to[ world[i] ];
+
+			// CONWAY'S RULES
+			if ( from < 4 ) { // DEAD
+				if ( sum === 3 ) { // tri-sexual reproduction
+					to = 4; // spawn new
 				} else {
-					if ( world[i] !== 0 ) {
-						world2[i] -= 1;
+					if ( world[i] !== 0 ) { // ghost
+						to -= 1; // more dead
 					}
 				}
-			} else {
-				if ( sum < 2 || sum > 3 ) {
-					world2[i] = 3;
+			} else { // ALIVE
+				if ( sum < 2 || sum > 3 ) { // too many or too few neighbors
+					to = 3; // die
 				}
+			}
+
+			// convert to dual-state key
+			switch ( from ) {
+				case 4:
+					if ( to === 4 ) {
+						world[i] = 0;
+					} else if ( to === 3 ) {
+						world[i] = 1;
+					}
+					break;
+				case 3:
+					if ( to === 4 ) {
+						world[i] = 2;
+					} else if ( to === 2 ) {
+						world[i] = 3;
+					}
+					break;
+				case 2:
+					if ( to === 4 ) {
+						world[i] = 4;
+					} else if ( to === 1 ) {
+						world[i] = 5;
+					}
+					break;
+				case 1:
+					if ( to === 4 ) {
+						world[i] = 6;
+					} else if ( to === 0 ) {
+						world[i] = 7;
+					}
+					break;
+				case 0:
+					if ( to === 4 ) {
+						world[i] = 8;
+					} else if ( to === 0 ) {
+						world[i] = 9;
+					}
+					break;
 			}
 		}
 
@@ -151,25 +195,21 @@ var goL = {
 		} else {
 			goL.hatch();
 		}
-		world = world2.slice();
-		world2 = [];
 		goL.render();
 	},
 	hatch: function() {
-		var re = new RegExp( "044.{"+(width-2)+"}440", "g" ),
-		    str = world2.join(""),
+		var re = new RegExp( "00.{"+(width-2)+"}00", "g" ),
+		    str = world.join(""),
 		    matches = [];
 		while ((match = re.exec(str)) != null) {
 		    matches.push( match.index );
 		}
 		str = "";
-		//console.log(matches);
 		eggs5 = eggs4.slice();
 		eggs4 = eggs3.slice();
 		eggs3 = eggs2.slice();
 		eggs2 = eggs1.slice();
 		eggs1 = matches.slice();
-		//eggs1 = matches.slice();
 
 		var hatch = [];
 
@@ -184,28 +224,14 @@ var goL = {
 			}
 		}
 
-		/*for ( var j=0; j<hatch.length; j++ ) {
-			var mRow = Math.floor( hatch[j] / width )
-				, mCol = Math.floor( hatch[j] % width );
-			for ( var k=0; k<world2.length; k++ ) {
-				var row = Math.floor( k/width )
-					, col = Math.floor( k%width );
-				if ( row === mRow  || col === mCol ) {
-					world2[k] = 4;
-				}
-			}
-		}*/
-
-		//console.log(matches);
-
 		var rand = random( 0, hatch.length ),
 			rRow = Math.floor( hatch[rand] / width ),
 			rCol = Math.floor( hatch[rand] % width );
-		for ( var j=0; j<world2.length; j++ ) {
+		for ( var j=0; j<world.length; j++ ) {
 			var row = Math.floor( j/width )
 				, col = Math.floor( j%width );
 			if ( row === rRow  || col === rCol ) {
-				world2[j] = 4;
+				world[j] = 0;
 			}
 		}
 		counter = 0;
