b1015bfd32c25c22acab214b08bfb685d87e3246
emanlapig <emmanlapig@gmail.com>
2016-10-30 18:25:31 -0400

    added options menu

--- Main.js
+++ Main.js
@@ -14,31 +14,60 @@ var width = Math.floor( window.innerWidth/unit )
 	, height = Math.floor( window.innerHeight/unit )
 	, total = width * height
 	, world = []
-	, fps = 12
+	, fps = 10
 	, playing
 	, canvas
 	, ctx
-	, bgColor = [random(0,255), random(0,255), random(0,255)]
-	, liveColor = [random(0,255), random(0,255), random(0,255)]
-	, deadColor = [random(0,255), random(0,255), random(0,255)]
 	, counter = 0
 	, eggs1 = []
 	, eggs2 = []
 	, eggs3 = []
 	, eggs4 = []
-	, eggs5 = [];
+	, eggs5 = []
+	, cycle_favs = true
+	, cycle_random = false
+	, colorIndex = 0
+	, cInterval = 10000
+	, cIntervals = [ 5000, 10000, 30000, 60000 ]
+	, cIntLabels = [ "5s", "10s", "30s", "60s" ]
+	, cIntIndex = 1
+	, colorCycle = setInterval( cycle_colors, cIntervals[ cIntIndex ] );
 
-// ghost color gradient
-var deadColor2 = [
-	Math.floor( Number( deadColor[0] + (bgColor[0] - deadColor[0])*(1/3) ) ),
-	Math.floor( Number( deadColor[1] + (bgColor[1] - deadColor[1])*(1/3) ) ),
-	Math.floor( Number( deadColor[2] + (bgColor[2] - deadColor[2])*(1/3) ) ),
-];
-var deadColor3 = [
-	Math.floor( Number( deadColor[0] + (bgColor[0] - deadColor[0])*(2/3) ) ),
-	Math.floor( Number( deadColor[1] + (bgColor[1] - deadColor[1])*(2/3) ) ),
-	Math.floor( Number( deadColor[2] + (bgColor[2] - deadColor[2])*(2/3) ) ),
-];
+var favorites = window.localStorage.getItem("GoL_colors");
+
+if ( favorites === null ) {
+	var bgColor = [255, 255, 255]
+		, liveColor = [0, 0, 0]
+		, deadColor = [255, 0, 0]
+		, deadColor2 = [
+			Math.floor( Number( deadColor[0] + (bgColor[0] - deadColor[0])*(1/3) ) ),
+			Math.floor( Number( deadColor[1] + (bgColor[1] - deadColor[1])*(1/3) ) ),
+			Math.floor( Number( deadColor[2] + (bgColor[2] - deadColor[2])*(1/3) ) ),
+		]
+		, deadColor3 = [
+			Math.floor( Number( deadColor[0] + (bgColor[0] - deadColor[0])*(2/3) ) ),
+			Math.floor( Number( deadColor[1] + (bgColor[1] - deadColor[1])*(2/3) ) ),
+			Math.floor( Number( deadColor[2] + (bgColor[2] - deadColor[2])*(2/3) ) ),
+		];
+	var defaultFavs = JSON.stringify({ array: [
+		{
+			bgColor: bgColor,
+			liveColor: liveColor,
+			deadColor: deadColor,
+			deadColor2: deadColor2,
+			deadColor3: deadColor3
+		}
+	]});
+	window.localStorage.setItem( "GoL_colors", defaultFavs );
+} else {
+	favorites = JSON.parse( window.localStorage.getItem("GoL_colors") );
+	shuffle( favorites.array );
+	var bgColor = favorites.array[0].bgColor
+		, liveColor = favorites.array[0].liveColor
+		, deadColor = favorites.array[0].deadColor
+		, deadColor2 = favorites.array[0].deadColor2
+		, deadColor3 = favorites.array[0].deadColor3;
+}
 
 
 // DUAL-STATE KEYS: Previously, we had to store 2 copies of the world--one to iterate over and one to output to.
@@ -50,6 +79,7 @@ var keyTo = [ 4, 3, 4, 2, 4, 1, 4, 0, 4, 0 ];
 var keyFrom = [ 4, 4, 3, 3, 2, 2, 1, 1, 0, 0 ];
 
 
+// GAME CONTROLLER
 var goL = {
 	init: function() {
 		canvas = document.getElementById( "stage" )
@@ -70,6 +100,85 @@ var goL = {
 				goL.start();
 			}
 		}
+		// button event listeners
+		canvas.addEventListener( "click", function( event ) {
+			var menu = document.getElementById( "menu" );
+			menu.setAttribute( "class", "hidden" );
+			var controls = document.getElementById( "controls" );
+			controls.setAttribute( "class", "" );
+		}, false );
+		var randomizeBtn = document.getElementById( "randomize" );
+		randomizeBtn.addEventListener( "click", random_color, false );
+		var openOptionsBtn = document.getElementById( "controls" );
+		openOptionsBtn.addEventListener( "click", function( event ) {
+			var menu = document.getElementById( "menu" );
+			menu.removeAttribute( "class" );
+			var controls = document.getElementById( "controls" );
+			controls.setAttribute( "class", "open" );
+		}, false );
+		var saveFavBtn = document.getElementById( "save-fav" );
+		saveFavBtn.addEventListener( "click", function( event ) {
+			var confirm = document.getElementById( "color-confirm" );
+			confirm.setAttribute( "class", "confirm" );
+		}, false );
+		var nextFavBtn = document.getElementById( "next-fav" );
+		nextFavBtn.addEventListener( "click", next_fav , false );
+		var yesBtn = document.getElementById( "yes" );
+		yesBtn.addEventListener( "click", function( event ) {
+			var colors = {
+				bgColor: bgColor,
+				liveColor: liveColor,
+				deadColor: deadColor,
+				deadColor2: deadColor2,
+				deadColor3: deadColor3
+			}
+			favorites.array.push( colors );
+			window.localStorage.setItem( "GoL_colors", JSON.stringify( favorites ) );
+			var confirm = document.getElementById( "color-confirm" );
+			confirm.setAttribute( "class", "confirm hidden" );
+		}, false );
+		var noBtn = document.getElementById( "no" );
+		noBtn.addEventListener( "click", function( event ) {
+			var confirm = document.getElementById( "color-confirm" );
+			confirm.setAttribute( "class", "confirm hidden" );
+		}, false );
+		var cycleFavsBtn = document.getElementById( "cycle-favs" );
+		var cycleRandomBtn = document.getElementById( "cycle-random" );
+		cycleFavsBtn.addEventListener( "click", function( event) {
+			if ( cycle_favs ) {
+				cycle_favs = false;
+				this.innerHTML = "cycle favs"
+				cycleRandomBtn.innerHTML = "cycle random";
+			} else {
+				cycle_favs = true;
+				cycle_random = false;
+				this.innerHTML = "&#10003; cycle favs"
+				cycleRandomBtn.innerHTML = "cycle random";
+			}
+			reset_cycle();
+		}, false );
+		cycleRandomBtn.addEventListener( "click", function( event) {
+			if ( cycle_random ) {
+				cycle_random = false;
+				this.innerHTML = "cycle random"
+				cycleFavsBtn.innerHTML = "cycle favs";
+			} else {
+				cycle_random = true;
+				cycle_favs = false;
+				this.innerHTML = "&#10003; cycle random"
+				cycleFavsBtn.innerHTML = "cycle favs";
+			}
+			reset_cycle();
+		}, false );
+		var cycleSpeedBtn = document.getElementById( "cycle-speed" );
+		cycleSpeedBtn.addEventListener( "click", function( event ) {
+			if ( cIntIndex < cIntervals.length-1 ) {
+				cIntIndex += 1;
+			} else {
+				cIntIndex = 0;
+			}
+			reset_cycle();
+		}, false );
 	},
 	render: function() {
 		ctx.restore(); // reset the canvas each frame
@@ -200,48 +309,10 @@ var goL = {
 		if ( counter < 100 ) {
 			counter += 1;
 		} else {
-			goL.hatch();
+			hatch();
 			counter = 0;
 		}
 		goL.render();
-	},
-	hatch: function() {
-		var re = new RegExp( "00.{"+(width-2)+"}00", "g" ), // "egg" search
-		    str = world.join(""),
-		    matches = [];
-		while ((match = re.exec(str)) != null) {
-		    matches.push( match.index );
-		}
-		str = "";
-		eggs5 = eggs4.slice();
-		eggs4 = eggs3.slice();
-		eggs3 = eggs2.slice();
-		eggs2 = eggs1.slice();
-		eggs1 = matches.slice();
-
-		var hatch = [];
-
-		for ( var j=0; j<eggs1.length; j++ ) {
-			var match2 = ( eggs2.indexOf( eggs1[j] ) > -1 )? 1 : 0
-				, match3 = ( eggs3.indexOf( eggs1[j] ) > -1 )? 1 : 0
-				, match4 = ( eggs4.indexOf( eggs1[j] ) > -1 )? 1 : 0
-				, match5 = ( eggs5.indexOf( eggs1[j] ) > -1 )? 1 : 0
-				, mature =  match2 + match3 + match4 + match5; 
-			if ( mature === 4 ) { // egg must survive 5 checks
-				hatch.push( eggs1[j] );
-			}
-		}
-
-		var rand = random( 0, hatch.length ), // choose one egg at random, if we have any
-			rRow = Math.floor( hatch[rand] / width ),
-			rCol = Math.floor( hatch[rand] % width );
-		for ( var j=0; j<world.length; j++ ) {
-			var row = Math.floor( j/width )
-				, col = Math.floor( j%width );
-			if ( row === rRow  || col === rCol ) { // generate a horizontal and vertical line
-				world[j] = 0;
-			}
-		}
 	},
 	start: function() {
 		if ( !fps ) {
@@ -258,6 +329,106 @@ var goL = {
 function random( a, b ) {
 	var c = b - a;
 	return Math.floor( Math.random()*c+a );
+}
+
+function hatch() {
+	var re = new RegExp( "00.{"+(width-2)+"}00", "g" ), // "egg" search
+	    str = world.join(""),
+	    matches = [];
+	while ((match = re.exec(str)) != null) {
+	    matches.push( match.index );
+	}
+	str = null;
+	// remember 5 checks
+	eggs5 = eggs4.slice();
+	eggs4 = eggs3.slice();
+	eggs3 = eggs2.slice();
+	eggs2 = eggs1.slice();
+	eggs1 = matches.slice();
+
+	var hatch = [];
+
+	for ( var j=0; j<eggs1.length; j++ ) {
+		var match2 = ( eggs2.indexOf( eggs1[j] ) > -1 )? 1 : 0
+			, match3 = ( eggs3.indexOf( eggs1[j] ) > -1 )? 1 : 0
+			, match4 = ( eggs4.indexOf( eggs1[j] ) > -1 )? 1 : 0
+			, match5 = ( eggs5.indexOf( eggs1[j] ) > -1 )? 1 : 0
+			, mature =  match2 + match3 + match4 + match5; 
+		if ( mature === 4 ) { // egg must survive 5 checks
+			hatch.push( eggs1[j] );
+		}
+	}
+
+	var rand = random( 0, hatch.length ), // choose one egg at random, if we have any
+		hRow = Math.floor( hatch[rand] / width ),
+		hCol = Math.floor( hatch[rand] % width );
+	for ( var j=0; j<world.length; j++ ) {
+		var row = Math.floor( j/width )
+			, col = Math.floor( j%width );
+		if ( row === hRow  || col === hCol ) { // generate a horizontal and vertical line of live cells
+			world[j] = 0;
+		}
+	}
+}
+
+function random_color() {
+	bgColor = [ Math.floor( random(0,255) ), Math.floor( random(0,255) ), Math.floor( random(0,255) ) ];
+	liveColor = [ Math.floor( random(0,255) ), Math.floor( random(0,255) ), Math.floor( random(0,255) ) ];
+	deadColor = [ Math.floor( random(0,255) ), Math.floor( random(0,255) ), Math.floor( random(0,255) ) ];
+	deadColor2 = [
+		Math.floor( Number( deadColor[0] + (bgColor[0] - deadColor[0])*(1/3) ) ),
+		Math.floor( Number( deadColor[1] + (bgColor[1] - deadColor[1])*(1/3) ) ),
+		Math.floor( Number( deadColor[2] + (bgColor[2] - deadColor[2])*(1/3) ) ),
+	];
+	deadColor3 = [
+		Math.floor( Number( deadColor[0] + (bgColor[0] - deadColor[0])*(2/3) ) ),
+		Math.floor( Number( deadColor[1] + (bgColor[1] - deadColor[1])*(2/3) ) ),
+		Math.floor( Number( deadColor[2] + (bgColor[2] - deadColor[2])*(2/3) ) ),
+	];
+}
+
+function cycle_colors() {
+	if ( cycle_favs ) {
+		next_fav();
+	} else if ( cycle_random ) {
+		random_color();
+	}
+}
+
+function next_fav() {
+	if ( colorIndex < favorites.array.length-1 ) {
+		colorIndex += 1;
+	} else {
+		colorIndex = 0;
+	}
+	bgColor = favorites.array[ colorIndex ].bgColor;
+	liveColor = favorites.array[ colorIndex ].liveColor;
+	deadColor = favorites.array[ colorIndex ].deadColor;
+	deadColor2 = favorites.array[ colorIndex ].deadColor2;
+	deadColor3 = favorites.array[ colorIndex ].deadColor3;
+}
+
+function shuffle( array ) {
+	var currentIndex = array.length, temporaryValue, randomIndex;
+	// While there remain elements to shuffle...
+	while (0 !== currentIndex) {
+		// Pick a remaining element...
+		randomIndex = Math.floor(Math.random() * currentIndex);
+		currentIndex -= 1;
+		// And swap it with the current element.
+		temporaryValue = array[currentIndex];
+		array[currentIndex] = array[randomIndex];
+		array[randomIndex] = temporaryValue;
+	}
+	return array;
+}
+
+function reset_cycle() {
+	cycle_colors();
+	clearInterval( colorCycle );
+	colorCycle = setInterval( cycle_colors, cIntervals[ cIntIndex ] );
+	var cycleSpeedBtn = document.getElementById( "cycle-speed" );
+	cycleSpeedBtn.innerHTML = "cycle speed " + cIntLabels[ cIntIndex ];
 }
 
 // the end.
