25bfa2b63872d4ed71edf4c23e950465fc6db963
emanlapig <emmanlapig@gmail.com>
2017-03-19 14:57:38 -0400

    updating clock styles

--- clock/clock.js
+++ clock/clock.js
@@ -7,17 +7,34 @@ var clock = {
 	minutes: 0,
 	seconds: 0,
 	interval: false,
+	military: true,
+	show_secs: true,
 	init: function() {
-		clock.interval = setInterval( function() {
-			time = new Date();
-			clock.hours = time.getHours();
-			clock.minutes = time.getMinutes();
-			clock.seconds = time.getSeconds();
-			var hour_ctnr = document.getElementById( "hours" ),
-				min_ctnr = document.getElementById( "minutes" ),
-				sec_ctnr = document.getElementById( "seconds" );
-			hour_ctnr.innerHTML = clock.hours;
-			min_ctnr.innerHTML = clock.minutes;
-		}, 1000 );
+		clock.update();
+		clock.interval = setInterval( clock.update, 1000 );
+	},
+	update: function() {
+		var hour_ctnr = document.getElementById( "hours" ),
+			min_ctnr = document.getElementById( "minutes" ),
+			sec_ctnr = document.getElementById( "seconds" );
+		time = new Date();
+		clock.hours = ( clock.military )? clock.zero_pad( time.getHours() ) : clock.zero_pad( clock.unmil( time.getHours() ) );
+		clock.minutes = clock.zero_pad( time.getMinutes() );
+		clock.seconds = clock.zero_pad( time.getSeconds() );
+		hour_ctnr.innerHTML = clock.hours;
+		min_ctnr.innerHTML = clock.minutes;
+		if ( clock.show_secs ) {
+			sec_ctnr.innerHTML = clock.seconds;
+		}
+	},
+	unmil: function( val ) {
+
+	},
+	zero_pad: function( val ) {
+		var pad = val.toString();
+		if ( val < 10 ) {
+			pad = "0" + val;
+		}
+		return pad;
 	}
-}
+};
